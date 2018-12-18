61e597fbac1ec877b31b7a9234986275c2f22603
c0deDAMN <nikiforov_f@mail.ru>
2018-12-18 23:18:47 +0300

    Inital commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wfm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'home-money';
+}
+
