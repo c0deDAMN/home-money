8563785ed7982436d7994c667ded6c2e99fd3ee6
Fil <f.nikiforov@gmail.com>
2018-12-19 17:14:22 +0300

    логин полностью готов
    готовый линки на страницу регистрации, добавлена логика при верном пользователе и пароле. сохранение локальной переменной

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { User } from 'src/app/shared/models/user.model';
 import { UsersService } from 'src/app/shared/services/users.service';
 import { Message } from 'src/app/shared/models/message.module';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'wfm-login',
@@ -14,7 +16,11 @@ export class LoginComponent implements OnInit {
   form: FormGroup;
   message: Message;
 
-  constructor(private userService: UsersService) { }
+  constructor(
+    private userService: UsersService,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.message = new Message('danger', '');
@@ -34,12 +40,15 @@ export class LoginComponent implements OnInit {
   onSubmit() {
     const formData = this.form.value;
 
-    // сделать кнопку неактивной пока не валидируется форма
+    // сделать чтобы ошибка не вылазила пока форма не валидируется
     this.userService.getUserByEmail(formData.email)
       .subscribe((user: User) => {
         if (user[0]) {    //костыль с индексом 
           if (user[0].password === formData.password) {
-            //logic
+            this.message.text = '';
+            window.localStorage.setItem('user', JSON.stringify(user));
+            this.authService.login();
+            // this.router.navigate(['']);
           } else {
             this.showMessage('Пароль не верный')
           }
