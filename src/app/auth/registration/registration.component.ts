6d917564711f623b5c0ecf39f6f643fa168b1bb0
Fil <f.nikiforov@gmail.com>
2018-12-19 11:09:53 +0300

    Create module AUTH

---
+++ src/app/auth/registration/registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wfm-registaration',
+  templateUrl: './registaration.component.html',
+  styleUrls: ['./registaration.component.scss']
+})
+export class RegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
