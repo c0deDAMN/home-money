ed001c0a9ad51af51dfd10f1a8df57866b470728
Fil <f.nikiforov@gmail.com>
2018-12-21 16:27:24 +0300

    add-event
    video 22

--- src/app/system/records-page/add-event/add-event.component.ts
+++ src/app/system/records-page/add-event/add-event.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import * as moment from 'moment';
+
+import { Category } from '../../shared/models/category.model';
+import { WfmEvent } from '../../shared/models/event.model';
 
 @Component({
   selector: 'wfm-add-event',
@@ -7,9 +12,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEventComponent implements OnInit {
 
+  @Input() categories: Category[] = [];
+  types = [ 
+    {type: 'income', label: 'Доход'},
+    {type: 'outcome', label: 'Расход'}
+  ]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    let {amount, description,category, type} = form.value;
+    if(amount < 0) amount *=-1;
+
+    var event = new WfmEvent(type, amount, +category, moment().format('DD.MM.YYYY HH:mm:ss'), description);
   }
 
 }
