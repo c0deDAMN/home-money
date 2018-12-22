40c70976e6f48a0564b8f87bb6b0b3f14b02c4b2
c0deDAMN <nikiforov_f@mail.ru>
2018-12-22 16:29:22 +0300

    chart
    video 26
    bug history page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { SystemModule } from './system/system.module';
     AppRoutingModule,
     AuthModule,
     HttpClientModule,
-    SystemModule
+    SystemModule,
+    BrowserAnimationsModule
   ],
   providers: [UsersService, AuthService],
   bootstrap: [AppComponent]
--- src/app/system/history-page/history-page.component.ts
+++ src/app/system/history-page/history-page.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CategoriesService } from '../shared/services/categories.service';
+import { EventService } from '../shared/services/events.service';
+import { Category } from '../shared/models/category.model';
+import { WfmEvent } from '../shared/models/event.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'wfm-history-page',
   templateUrl: './history-page.component.html',
   styleUrls: ['./history-page.component.scss']
 })
-export class HistoryPageComponent implements OnInit {
+export class HistoryPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  isLoaded1 = false;
+  isLoaded2 = false;
+
+  s1: Subscription;
+  s2: Subscription;
+
+  constructor(private categoriesService: CategoriesService,
+    private eventsService: EventService) { }
+
+  categories: Category[] = [];
+  events: WfmEvent[] = [];
+
+  chartData = [];
 
   ngOnInit() {
+    this.s1 = this.categoriesService.getCategories().subscribe((date: Category[]) => {
+      this.categories = date;
+      this.isLoaded1 = true;
+    });
+
+    this.s2 = this.eventsService.getEvents().subscribe((date: WfmEvent[]) => {
+      this.events = date;
+      this.isLoaded2 = true;
+      this.calculateChartData();
+    });
+  }
+
+  calculateChartData(): void {
+    this.chartData = [];
+
+    console.log(this.isLoaded1)
+    if (!this.isLoaded1) {
+      console.log("sorry:-/(");
+    }
+
+    this.categories.forEach((cat) => {
+      const catEvent = this.events.filter((e) => e.category === cat.id && e.type === 'outcome');
+      this.chartData.push({
+        name: cat.name,
+        value: catEvent.reduce((total, e) => {
+          total += e.amount;
+          return total;
+        }, 0)
+      })
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.s1) this.s1.unsubscribe();
+    if (this.s2) this.s2.unsubscribe();
   }
 
 }
